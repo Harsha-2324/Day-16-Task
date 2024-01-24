7143b797e2142e00bd65ef173aea32c5329ce49c
Harsha-2324 <rharshva@gmail.com>
2024-01-24 19:16:14 +0530

    CALLBACK HELL Commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("displayCountdown").innerText = "10";
+  setTimeout(() => {
+    document.getElementById("displayCountdown").innerText = "9";
+    setTimeout(() => {
+      document.getElementById("displayCountdown").innerText = "8";
+      setTimeout(() => {
+        document.getElementById("displayCountdown").innerText = "7";
+        setTimeout(() => {
+          document.getElementById("displayCountdown").innerText = "6";
+          setTimeout(() => {
+            document.getElementById("displayCountdown").innerText = "5";
+            setTimeout(() => {
+              document.getElementById("displayCountdown").innerText = "4";
+              setTimeout(() => {
+                document.getElementById("displayCountdown").innerText = "3";
+                setTimeout(() => {
+                  document.getElementById("displayCountdown").innerText = "2";
+                  setTimeout(() => {
+                    document.getElementById("displayCountdown").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("displayCountdown").innerText =
+                        "Happy Independence Day!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
